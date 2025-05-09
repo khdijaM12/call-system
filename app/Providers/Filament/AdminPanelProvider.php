c20f24be449a9c1ed4f85427f0b13a9cbe25175e
khdija <khdija@gmail>
2025-05-09 18:32:01 +0300

    Theme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,13 +24,14 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->brandName('call system')
             ->id('admin')
             ->path('admin')
             ->darkMode(false)
             ->registration()
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Sky,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
