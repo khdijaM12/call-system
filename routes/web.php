7f8b56bb918f07d2e8c2685ca224c5171d9e51ba
tahanijoail <tahaijoail@gmail.com>
2025-05-09 22:11:48 +0300

    profile calls and employees

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,13 @@ Route::get('/', function () {
 });
 
 Route::get('/counter', Counter::class);
+
+use App\Http\Controllers\Admin\ReportController;
+
+
+Route::middleware(['auth'])->group(function () {
+    
+    Route::get('/admin/reports', [ReportController::class, 'index'])->name('admin.reports');
+    Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+
+});
